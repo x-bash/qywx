b058b9f8fd1a21ab68adebc390156d95af692528
tzw <tangzhiwen@x-cmd.com>
2022-07-19 17:20:11 +0800

    update

--- test/dingtalk.test.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-. dingtalk
-
-dingtalk.bot.new a "https://oapi.dingtalk.com/robot/send?access_token=8a87f39c58cf15142a6d3728eb6eef7baa793632098bcc1d0a62ab1c61724e8a"
-a.send_text_msg hi
-
-
-a.send_text_msg -h
